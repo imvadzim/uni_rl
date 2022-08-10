7d811fb314d0e6fc68fbf747b2cfef0a7e6d2d4e
Vadim Novikau <5254220@gmail.com>
2022-08-10 18:40:20 +0300

    python==3.5.5

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ source /home/vadzim/miniconda3/etc/profile.d/conda.sh
 conda create -n uni_rl -y
 conda activate uni_rl
 pip install git+https://github.com/jupyter/notebook.git@4.3.1
-conda install python==3.5.6 -y
+conda install python==3.5.5 -y
 git clone https://github.com/openai/universe.git
 cd universe
 pip install -e .
