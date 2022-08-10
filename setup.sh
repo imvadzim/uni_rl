ce7e14711553e4666afb6e225fec29368541888c
Vadim Novikau <5254220@gmail.com>
2022-08-10 20:03:08 +0300

    notebook==6.1.6, python==3.9.12: failure to connect with kernel

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ source /home/vadzim/miniconda3/etc/profile.d/conda.sh
 conda create -n uni_rl -y
 conda activate uni_rl
 conda install notebook==6.1.6 -y
-conda install python==3.6.13 -y
+conda install python==3.9.12 -y
 # pip install numpy
 # conda install go -y
 git clone https://github.com/openai/universe.git
