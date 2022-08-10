eae7537d8ab7600520e4751abfb1a51b91774304
Vadim Novikau <5254220@gmail.com>
2022-08-10 14:58:28 +0300

    old versions of notebook have issues

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 source /home/vadzim/miniconda3/etc/profile.d/conda.sh
 conda create -n uni_rl -y
 conda activate uni_rl
-pip install git+https://github.com/jupyter/notebook.git@4.4.0
+conda install notebook==5.0.0 -y
+conda install python==3.5.4 -y
 git clone https://github.com/openai/universe.git
 cd universe
 pip install -e .
 pip install gym==0.9.5
 cd ../../gym-examples/
-conda install ipykernel -y
-ipython kernel install --name my_env --user
+# conda install ipykernel -y
+# ipython kernel install --name my_env --user
 jupyter notebook
