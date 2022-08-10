f401b8b50bb74354e6294fdbba63492a341c54bc
Vadim Novikau <5254220@gmail.com>
2022-08-10 18:30:20 +0300

    different versions of notebook and python

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 source /home/vadzim/miniconda3/etc/profile.d/conda.sh
 conda create -n uni_rl -y
 conda activate uni_rl
-conda install notebook==6.1.4 -y
-conda install python==3.7.13 -y
+pip install git+https://github.com/jupyter/notebook.git@4.3.1
+conda install python==3.5.6 -y
 git clone https://github.com/openai/universe.git
 cd universe
 pip install -e .
