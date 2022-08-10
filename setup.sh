216939199bbc7207ea3b14e297182989c9943904
Vadim Novikau <5254220@gmail.com>
2022-08-10 13:44:47 +0300

    attempt 1

--- setup.sh
+++ setup.sh
@@ -3,9 +3,12 @@ source /home/vadzim/miniconda3/etc/profile.d/conda.sh
 conda create -n uni_rl -y
 conda activate uni_rl
 conda install notebook -y
-git clone https://github.com/openai/universe.git`:
+git clone https://github.com/openai/universe.git
 cd universe
 pip install -e .
-cd ../
-conda install ipykernel
-ipython kernel install --name my_env --user
+pip install gym==0.9.5
+conda install python==3.9.12 -y
+cd ../gym-examples/
+# conda install ipykernel
+# ipython kernel install --name my_env --user
+jupyter notebook
