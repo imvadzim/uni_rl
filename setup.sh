03f55470bc734203c44b12641a1e29c941c3b842
Vadim Novikau <5254220@gmail.com>
2022-08-10 13:53:51 +0300

    attempt 2, trying to fix a failure with coonection to kernel

--- setup.sh
+++ setup.sh
@@ -2,12 +2,11 @@
 source /home/vadzim/miniconda3/etc/profile.d/conda.sh
 conda create -n uni_rl -y
 conda activate uni_rl
-conda install notebook -y
+pip install git+https://github.com/jupyter/notebook.git@4.4.0
 git clone https://github.com/openai/universe.git
 cd universe
 pip install -e .
 pip install gym==0.9.5
-conda install python==3.9.12 -y
 cd ../gym-examples/
 # conda install ipykernel
 # ipython kernel install --name my_env --user
